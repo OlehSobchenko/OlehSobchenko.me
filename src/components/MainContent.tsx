5e28c505bacdefa37f651f149c03d69c5cdfe0f7
Serhiy Morenko <serhiy.greench@gmail.com>
2025-06-01 12:39:24 +0200

    wip: fixing deployment

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,4 +1,3 @@
-import './globals.css';
 import portrait from '@/img/main-photo.svg';
 import PersonInfo from '@/components/specific/PersonInfo';
 import ScrollDown from '@/components/specific/ScrollDown';
@@ -9,7 +8,7 @@ export default async function MainContent() {
     return <div className="relative" id="main-content">
         <div
             id="main-background"
-            className={ 'main-background-photo h-screen bg-no-repeat lg:bg-size-[65vh] bg-size-[50vh] bg-position-y-[calc(100% + 2px)] lg:bg-position-x-[75%] bg-position-x-[50%]' }
+            className={ 'lg:bg-[75%_calc(100%+2px)] bg-[center_calc(100%+2px)] h-screen bg-no-repeat lg:bg-size-[65vh] bg-size-[50vh] bg-position-y-[calc(100% + 2px)] lg:bg-position-x-[75%] bg-position-x-[50%]' }
             style={ {
                 backgroundImage: `linear-gradient(to bottom, var(--gradient-background-color-start) 80%, var(--gradient-background-color-end)), url("${ portrait.src }")`,
             } }
