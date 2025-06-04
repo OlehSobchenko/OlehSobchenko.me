ba230b97c532af23874ffbfb4d13424bd21dee4a
Serhiy Morenko <serhiy.greench@gmail.com>
2025-06-05 00:54:57 +0200

    fix: metadata for links preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,13 +54,37 @@ export default async function RootLayout(
     { children }: PropsWithChildren,
 ) {
     const locale = await getLocale();
+    const t = await getTranslations('main');
 
     return <html lang={ locale } suppressHydrationWarning>
         <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+            />
+            <link rel="manifest" href="site.webmanifest"/>
+            <meta name="theme-color" content="#000000"/>
+            <link rel="manifest" href="/site.webmanifest"/>
+            <link
+                rel="mask-icon"
+                href="/safari-pinned-tab.svg"
+                color="#000000"
+            />
+            <meta name="msapplication-TileColor" content="#000000"/>
+            <meta property="og:title" content={ t('title') }/>
+            <meta property="og:description" content={ t('description') }/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content="https://olehsobchenko.me"/>
+            <meta
+                property="og:image"
+                content="https://olehsobchenko.me/android-chrome-512x512.png"
+            />
+            <meta property="og:image:alt" content="/favicon.ico"/>
         </head>
         <body
-            className={ `${ robotoCondensed.className } bg-main-background text-main-color font-bold fill-main-color overflow-x-hidden` }
+            className={
+                `${ robotoCondensed.className } bg-main-background text-main-color font-bold fill-main-color overflow-x-hidden`
+            }
             suppressHydrationWarning
         >
             <ThemeProvider>
