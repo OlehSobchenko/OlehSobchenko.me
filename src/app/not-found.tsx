fcb87d1405ba64b84e363768532ebb9b8930afcf
Serhiy Morenko <serhiy.greench@gmail.com>
2025-11-24 17:31:25 +0100

    fix: newly added posts are unavailable

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,25 @@
-import MainPage from '@/components/pages/MainPage';
+'use client';
 
-export default function NotFound() {
+import { useEffect, useState } from 'react';
+import MainPage from '@/components/pages/MainPage';
+import PostPage from '@/components/pages/PostPage';
+
+export default function Custom404() {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return null;
+    }
+
+    const [path, id] = window.location.pathname.split('/').filter(i => i);
+
+    if (['post', 'p'].includes(path) && id) {
+        return <PostPage id={ id }/>;
+    }
+
     return <MainPage/>;
 }
