3a33d8d055b8d4a6c968a8b32366ebc83dd0df74
Serhiy Morenko <67065565+SerhiyGreench@users.noreply.github.com>
2025-11-25 12:32:24 +0100

    Update AudioPlayerProvider.tsx

--- src/components/providers/AudioPlayerProvider.tsx
+++ src/components/providers/AudioPlayerProvider.tsx
@@ -59,8 +59,8 @@ export const AudioPlayerProvider = (
                 .map(enrichAudio)
                 .toSorted(
                     (a, b) =>
-                        +(a?.priority || Infinity) -
-                        +(b?.priority || Infinity));
+                        +(b?.priority || Infinity) -
+                        +(a?.priority || Infinity));
 
             setTracks(enrichedTracks as Audio[]);
 
